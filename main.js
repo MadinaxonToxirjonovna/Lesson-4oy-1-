05f0bf2f2a4f713c2dd6b19d31e99e450a60c7b4
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-02-28 17:02:57 +0500

    Lesson 4oy (1)

---
+++ main.js
@@ -0,0 +1,24 @@
+// console.log("Hello word!!!");
+
+// alert("Miss.Xon");
+
+let Name = prompt("Ismingizni kiriting")
+console.log("Name = " + Name)
+
+
+let Lastname = prompt("Familyangizni kiriting")
+console.log("Lastname = " +Lastname)
+
+
+let Aga = prompt("Yoshingizni kiritng")
+console.log("Age = " +Aga)
+
+let Fan = prompt("Yoqtirgan faningizni yozing")
+console.log("Fan = " +Fan)
+
+let Tuman = prompt("Yashaydigan tumaningizni kiriting")
+console.log("Tuman = " +Tuman)
+
+// alert("\n Ismingizni kiriting = " + Name + "  \n Familyangizni kiriting = " + Lastname + "  \n Yoshingizni kiritng = " + Aga+ "  \n Yoqtirgan faningizni yozing = " +Fan+ " \n Yashaydigan tumaningizni kiriting =" +Tuman+ " \n ")
+
+alert("Malumotni kiritganingiz uchun tashakkur!")
